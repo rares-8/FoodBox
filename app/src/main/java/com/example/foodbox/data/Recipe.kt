f2c5024dafa724c1e90e65674598a213d8024be1
Rares Apostol <apostol.rares003@gmail.com>
2024-10-05 12:31:22 +0300

    first commit

---
+++ app/src/main/java/com/example/foodbox/data/Recipe.kt
@@ -0,0 +1,9 @@
+package com.example.foodbox.data
+
+data class Recipe(
+    val id: Int = 0,
+    val name: String,
+    val ingredients: String,
+    val instructions: String,
+    val price: Int = -1,
+)
---
+++ app/src/main/java/com/example/foodbox/ui/home/HomeScreen.kt
@@ -0,0 +1,6 @@
+package com.example.foodbox.ui.home
+
+
+class HomeScreen {
+
+}
